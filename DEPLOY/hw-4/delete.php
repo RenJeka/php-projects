4ff6f726454eb8f2c50abfcea61ace496eaa40c2
RenJeka <renjeka@gmail.com>
2020-10-14 16:36:43 +0300

    /structure DEPLOY-> hw-4 all views are placed in a special folder

--- DEPLOY/hw-4/delete.php
+++ DEPLOY/hw-4/delete.php
@@ -10,43 +10,5 @@ if ($articleId > 0) {
 } else {
     array_push($errors, "Неправильный ID! Попробуйте удалить статью снова.");
 }
-?>
 
-<!DOCTYPE html>
-<html lang="ru">
-
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Удаление статьи</title>
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha2/css/bootstrap.min.css"
-		integrity="sha384-DhY6onE6f3zzKbjUPRc2hOzGAdEf4/Dz+WJwBvEYL/lkkIsI3ihufq9hk9K4lVoK" crossorigin="anonymous">
-
-	<style>
-	* {
-		font-size: 18px;
-	}
-	</style>
-</head>
-
-<body>
-	<div class="container">
-		<div class="row">
-			<div class="col-md-12 ">
-				<div class="d-flex flex-column justify-content-center align-items-center my-5">
-					<? if ($isDeleted): ?>
-					<h1 class="display-3 text-center">Статья удалена!</h1>
-					<? endif; ?>
-					<? foreach ($errors as  $error): ?>
-					<p class="error"><?= $error ?></p>
-					<? endforeach; ?>
-					<div class="my-3">
-                        <a href="index.php" class="btn btn-primary btn-lg">На главную</a>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-</body>
-
-</html>
+include "views/v_delete.php";
